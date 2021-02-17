b7b5ca8bcaf9f65104513d7fec1991a0884329dd
Amy <amytang77@gmail.com>
2021-02-17 17:57:26 -0500

    slideshow state management | add looping fix | index linted

--- src/controllers/slideshow_controller.js
+++ src/controllers/slideshow_controller.js
@@ -1,26 +1,36 @@
-import { Controller } from 'stimulus'
+import { Controller } from 'stimulus';
 
 export default class extends Controller {
-  static targets = ["slide"]
+  static targets = ['slide'];
+  static values = { index: Number };
 
-  initialize(){
-    this.index = 0
-    this.showCurrentSlide()
-  }
+  // initialize() {
+  //   // this.index = 0;
+  //   // this.index = this.indexValue;
+  //   this.showCurrentSlide();
+  // }
 
   next() {
-    this.index++
-    this.showCurrentSlide()
+    this.indexValue++;
+    if (this.indexValue > this.slideTargets.length - 1) {
+      this.indexValue = 0;
+    }
   }
 
   previous() {
-    this.index--
-    this.showCurrentSlide()
+    this.indexValue--;
+    if (this.indexValue < 0) {
+      this.indexValue = this.slideTargets.length - 1;
+    }
+  }
+
+  indexValueChanged() {
+    this.showCurrentSlide();
   }
 
   showCurrentSlide() {
-    this.slideTargets.forEach((element, index) => {
-      element.hidden = index != this.index
-    })
+    this.slideTargets.map((element, index) => {
+      element.hidden = index != this.indexValue;
+    });
   }
 }
